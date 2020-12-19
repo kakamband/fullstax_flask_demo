1da8e3befe80803773d74b1bf1bcc4bb8f70c9cb
techcheese <cesartole73@gmail.com>
2020-12-19 14:45:12 -0600

    add requirements

--- prices/static/js/price_graph.js
+++ prices/static/js/price_graph.js
@@ -1,15 +1,29 @@
+
+
+d3.json(`/btc_data/${JSstart}/${JSclose}`, data =>{
+    objects = Object.values(data)
+
+    console.log(objects)
+
+    time = objects.map(x => x.unix)
+    openn = objects.map( x => x.open)
+    console.log(openn)
+
+
 var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
+    x: time,
+    y: openn,
     type: 'scatter'
   };
   
-  var trace2 = {
-    x: [1, 2, 3, 4],
-    y: [16, 5, 11, 9],
-    type: 'scatter'
-  };
+//   var trace2 = {
+//     x: [1, 2, 3, 4],
+//     y: [16, 5, 11, 9],
+//     type: 'scatter'
+//   };
   
-  var data = [trace1, trace2];
+  var traces = [trace1];
   
-  Plotly.newPlot('pricePlot', data);
+  Plotly.newPlot('pricePlot', traces);
+
+})
--- prices/views.py
+++ prices/views.py
@@ -1,7 +1,36 @@
+import os
 from . import app
+import pandas as pd
 
 from flask import render_template, jsonify
 
 @app.route('/prices')
 def check_price():
     return render_template('graph.html')
+
+@app.route('/prices/<start>/<close>')
+def check_price_range(start, close):
+
+    return render_template('graph.html', start=start, closes=close)
+
+
+
+@app.route('/btc_data')
+def btc():
+
+    df = pd.read_csv('./prices/static/assets/Coinbase_BTCUSD.csv')
+
+    df_dict = df.T.to_dict()
+
+    return jsonify(df_dict)
+
+
+@app.route('/btc_data/<start>/<close>')
+def btc_range(start, close):
+
+    df = pd.read_csv('./prices/static/assets/Coinbase_BTCUSD.csv')
+    df['unix'] = df['unix'].astype(int)
+    df = df[(df['unix'] > int(start)) & (df['unix'] < int(close))]
+    df_dict = df.T.to_dict()
+
+    return jsonify(df_dict)
